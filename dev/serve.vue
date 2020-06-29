9835cf352c73f72a907708b7db3ed58f9606b4ba
Mark Stoumann <mark@stoumann.dk>
2020-06-29 22:44:17 +0200

    Started the wizard

---
+++ dev/serve.vue
@@ -0,0 +1,21 @@
+<script>
+	import Vue from 'vue';
+	import { VueWizard, TabContent } from '@/entry';
+
+	export default Vue.extend({
+		name: 'ServeDev',
+		components: {
+			VueWizard, TabContent
+		}
+	});
+</script>
+
+<template>
+    <div id="app">
+        <vue-wizard>
+			<tab-content title="Stor ostemad">Ostemad</tab-content>
+			<tab-content title="Dunser">Frikadelle</tab-content>
+			<tab-content title="Juleaften!">Flæskesteg</tab-content>
+		</vue-wizard>
+    </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+import VueWizard from './vue-wizard.vue';
+import TabContent from './tab-content.vue';
+import WizardTab from './wizard-tab.vue';
+
+export { VueWizard, TabContent, WizardTab };
