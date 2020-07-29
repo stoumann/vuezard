47235b478f0b94223496d5ebf43f9f0c0db450f5
Mark Stoumann <mark@stoumann.dk>
2020-07-29 14:17:25 +0200

    Changed name of component and edited the readme to fit the changes

--- src/lib-components/vuezard.vue
+++ src/lib-components/vuezard.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="vue-wizard">
+	<div class="vuezard">
 		<div class="wizard-header" v-if="title">
 			<h4 class="wizard-title">{{title}}</h4>
 			<p class="wizard-subtitle">{{subTitle}}</p>
@@ -465,109 +465,5 @@
 </script>
 
 <style>
-	.vue-wizard {
-		position: relative;
-		border: 1px solid #EDF2F7;
-		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; /* TODO: REMOVE THIS */
-	}
-
-	.vue-wizard .wizard-header {
-		display: flex;
-		align-items: baseline;
-	}
-
-	.vue-wizard .wizard-header .wizard-title {
-		font-size: 24px;
-		margin: 0 10px 0 0;
-		padding: 0;
-	}
-
-	.vue-wizard .wizard-header .wizard-subtitle {
-		font-size: 14px;
-		color: #999;
-	}
-
-	.vue-wizard .wizard-container {
-		display: flex;
-		align-items: stretch;
-		height: 100%;
-	}
-
-	.vue-wizard .wizard-container .wizard-tabs .wizard-tablist { 
-		list-style: none;
-		margin: 0;
-		padding: 0;
-		height: 100%;
-		background: #F7FAFC;
-		border-right: 1px solid #EDF2F7;
-	}
-
-	/* .vue-wizard .wizard-container .wizard-tabs .wizard-tablist li {
-
-	} */
-
-	.vue-wizard .wizard-container .wizard-tabs .wizard-tablist li a {
-		display: block;
-		border-bottom: 1px solid #EDF2F7;
-		text-decoration: none;
-	}
-
-	.vue-wizard .wizard-container .wizard-tabs .wizard-tablist li a.disabled {
-		color: #999;
-		cursor: not-allowed;
-	}
-
-	.vue-wizard .wizard-container .wizard-tabs .wizard-tablist li:last-child a {
-		border-bottom: 0;
-	}
-
-	.vue-wizard .wizard-container .wizard-tabs .wizard-tablist .wizard-tab {
-		margin: 0;
-		padding: 10px;
-	}
-
-	.vue-wizard .wizard-container .wizard-content-footer {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-	}
-
-	.vue-wizard .wizard-container .wizard-content-footer .wizard-content {
-		margin: 0;
-		padding: 10px;
-		display: flex;
-		flex: 1;
-	}
-	.vue-wizard .wizard-container .wizard-content-footer .wizard-content {
-		margin: 0;
-		padding: 10px;
-	}
 	
-	.vue-wizard .wizard-container .wizard-content-footer .wizard-card-footer {
-		padding: 10px;
-		background: #F7FAFC;
-		border-top: 1px solid #EDF2F7;
-		display: flex;
-		justify-content: space-between;
-	}
-	
-	/* .vue-wizard .wizard-container .wizard-content-footer .wizard-card-footer .wizard-footer-left,
-	.vue-wizard .wizard-container .wizard-content-footer .wizard-card-footer .wizard-footer-right {
-	} */
-
-	.vue-wizard .wizard-container .wizard-content-footer .wizard-card-footer .wizard-footer-left button,
-	.vue-wizard .wizard-container .wizard-content-footer .wizard-card-footer .wizard-footer-right button {
-		background: #2D3748;
-		border: none;
-		color: #ffffff;
-		padding: 4px 12px;
-		border-radius: 4px;
-		cursor: pointer;
-		font-size: 12px;
-		outline: none;
-	}
-
-	.vue-wizard .wizard-container .wizard-content-footer .wizard-card-footer .wizard-footer-right button.finish-button {
-		background: #f07d00 !important;
-	}
 </style>
